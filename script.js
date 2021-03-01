e775d93e90bfd147530dc551cb18adce3a9caea7
flpglw <79870240+flpglw@users.noreply.github.com>
2021-03-01 21:32:05 +0100

    re

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let buttons = document.querySelectorAll('button')
 
 new ClipboardJS(buttons);
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
