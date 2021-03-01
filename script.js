21a8d05aa260ed06d9731cc35a80e13fff199739
flpglw <79870240+flpglw@users.noreply.github.com>
2021-03-01 21:35:26 +0100

    A

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let buttons = document.querySelectorAll('button')
 new ClipboardJS(buttons);
 
 function myFunction() {
-  var x = document.getElementById("myDIV");
+  var x = document.getElementById("divRetail");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
